3186a1bade2742505dead1c6dfbbbaaaaa17285d
hahamik <dlapdl0615@gmail.com>
2025-04-02 16:02:59 +0900

    blog-ajax-server 서버준비

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,5 +1,8 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
-
-    private final BoardRepository boardRepository;
 
     @GetMapping("/")
     public String index() {
@@ -22,7 +23,16 @@ public class BoardController {
     }
 
     @GetMapping("/board/{id}/updateForm")
-    public String updateForm(@PathVariable int id) {
+    public String updateForm(@PathVariable int id, HttpServletRequest request, HttpServletResponse response) {
+        // 1. ssr : template engine
+        request.setAttribute("boardId", id);
+        // 2. cookie
+        Cookie cookie = new Cookie("boardId", id + "");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+        // 응답의 헤더 -> Set-Cookie : boardId=5
+
+
         return "board/updateForm";
     }
 }
